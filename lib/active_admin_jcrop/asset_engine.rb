a11e53e5a34f793ce96cbe3cfd2907d9619ed5ca
Zane Shannon <zcs@smileslaughs.com>
2015-07-10 14:59:54 -0700

    just dont use carrierwave okay.

--- lib/active_admin_jcrop/asset_engine.rb
+++ lib/active_admin_jcrop/asset_engine.rb
@@ -1,5 +1,3 @@
-if Gem::Specification::find_all_by_name('carrierwave').any?
-  require 'active_admin_jcrop/asset_engine/carrier_wave'
-elsif Gem::Specification::find_all_by_name('paperclip').any?
+if Gem::Specification::find_all_by_name('paperclip').any?
   require 'active_admin_jcrop/asset_engine/paperclip'
 end
